b17dd0e492763157de8d1a7189547659dafef175
alvarodavisantos <129607745+alvarodavisantos@users.noreply.github.com>
2023-06-23 17:14:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "white"];
+//const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "white"];
+const colors = [
+  "green",
+  "red",
+  "rgba(133,122,200,0.8)",
+  "#f15025",
+  "white",
+  "blue",
+  "yellow",
+  "pink",
+  "#aabbcc",
+  "purple",
+  "orange",
+  "rgba(255,0,0,0.5)",
+  "cyan",
+  "magenta",
+  "#ff6600",
+  "black",
+  "brown",
+  "rgba(0,255,255,0.7)",
+  "#912c8f",
+  "gold",
+  "rgba(128,128,128,0.3)",
+  "#9932cc",
+  "teal",
+  "rgba(255,165,0,0.6)",
+  "#00ffff",
+  "silver",
+  "maroon",
+  "#006400",
+  "navy",
+  "rgba(255,255,0,0.4)"
+];
+
 const btn = document.getElementById("btn");
 
 btn.addEventListener('click', function(){
